2f9504da38531725c9d3ecf078ff19acb8da0a55
Neil Fenton <neilff@gmail.com>
2016-01-28 21:18:41 -0500

    added component boilerplate

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React, { PropTypes } from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      ...
+    </div>
+  );
+};
+
+Card.propTypes = {
+  // ...
+};
+Card.defaultProps = {
+  // ...
+};
+
+export default Card;
