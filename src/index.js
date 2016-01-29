b3d27e4c8aac48d73997dfe8b7f7e81dccb4cc4c
Neil Fenton <neilff@gmail.com>
2016-01-28 21:18:41 -0500

    added component boilerplate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Card from './components/Card';
+
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <Card />,
   document.getElementById('root')
 );
