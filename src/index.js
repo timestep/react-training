3ccc334947f154702a2754f31d6fc84a480c8dff
Neil Fenton <neilff@gmail.com>
2016-02-10 22:35:05 -0500

    added simpler redux example

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
 
-import * as reducers from './reducers/index';
+export const INCREASE = '@@reactTraining/INCREASE';
+export const DECREASE = '@@reactTraining/DECREASE';
 
-const rootReducer = combineReducers(reducers);
-const store = createStore(rootReducer, {});
+const INITIAL_STATE = 0;
 
-import App from './containers/App';
+function counterReducer(state = INITIAL_STATE, action = {}) {
+  switch (action.type) {
+    case INCREASE:
+      return state + 1;
 
-ReactDOM.render(
-  <Provider store={ store }>
-    <KitchenSink />
-  </Provider>,
-  document.getElementById('root')
-);
+    case DECREASE:
+      return state - 1;
+
+    default:
+      return state;
+  }
+}
+
+export function increase() {
+  return { type: INCREASE };
+}
+
+export function decrease() {
+  return { type: DECREASE };
+}
+
+const rootReducer = combineReducers({ counter: counterReducer });
+const store = createStore(rootReducer);
+
+store.subscribe(() => {
+  ReactDOM.render(
+    <div>
+      <pre>{ JSON.stringify(store.getState(), null, 2) }</pre>
+      <button onClick={ () => store.dispatch(increase()) }>Increase</button>
+      <button onClick={ () => store.dispatch(decrease()) }>Decrease</button>
+    </div>,
+    document.getElementById('root')
+  );
+});
+
+store.dispatch({ type: 'INIT' });
